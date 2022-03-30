685357380046e1d4ba9d8e8a1c273042371d907c
josheking18 <81638131+josheking18@users.noreply.github.com>
2022-03-30 14:38:53 -0300

    Netflix

---
+++ netflix/Rapido.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rapido Y Furioso 9</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<header>
+		<div class="contenedor">
+			<h2 class="logotipo">Joshe_king18</h2>
+			<nav>
+				<a href="#" class="activo">Inicio</a>
+				<a href="#">Programas</a>
+				<a href="#">Películas</a>
+				<a href="#">Más Recientes</a>
+				<a href="#">Mi lista</a>
+			</nav>
+		</div>
+	</header>
+    
+</body>
+</html>
